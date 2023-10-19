606ae3174db77ba7b18c4429db7bc572eded4572
nancyiet <nancynancybansal@gmail.com>
2023-10-19 20:20:04 +0530

    Card shape Changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import AddProject from './AddProject';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
   <div className="app">
-    <AddProject/>
+    <Dashboard/>
   </div>
   );
 }
--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -28,7 +28,7 @@ export default function ProjectCard(props) {
     
   return (
     <Box className="box" sx={{  margin: 5 }} >
-      <Card variant="outlined" className='shadow-md'>
+      <Card variant="outlined" className='shadow-md rounded-lg'>
       
         <CardContent>
           <Typography variant="h5"  gutterBottom>
@@ -51,7 +51,12 @@ export default function ProjectCard(props) {
            
         </CardContent>
         <CardActions>
-          <Button size="small">More Details</Button>
+        <button
+						type="button"
+						className="w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+					>
+						More Details
+					</button>
         </CardActions>
       
         </Card>
