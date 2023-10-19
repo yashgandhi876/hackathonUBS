35682747fdadc34a0baeabdbee957c3b805a261a
Yash Gandhi <yashgandhi2020@gmail.com>
2023-10-19 19:02:16 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Dashboard from './Dashboard';
-import Header  from './Header';
 import AddProject from './AddProject';
 
 function App() {
   return (
   <div className="app">
-    <Header/>
-    <Dashboard/>
     <AddProject/>
   </div>
   );
